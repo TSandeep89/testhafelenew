7f5403d386ff44d6a7df2d3447481563631f10a5
sandeep <tsandy011@gmail.com>
2022-06-15 12:44:28 +0530

    Article to Cart uploaded

--- src/test/java/stepdefination/ArticletoCartPage.java
+++ src/test/java/stepdefination/ArticletoCartPage.java
@@ -44,7 +44,7 @@ public class ArticletoCartPage extends BaseClass {
 
         }
         catch(Exception e) {
-            System.out.println("Something went wrong.");
+            System.out.println("Something went wrong");
         }
 
     }
